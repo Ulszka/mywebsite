6400ecba33fd17cca49f1d18748252e27a6f5b7d
Ulszka <ulszkahand@gmail.com>
2025-06-16 22:51:41 +0200

    Updating home page layout

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,30 +1,34 @@
 import "./home.scss";
-import { BrowserRouter as Route, NavLink } from "react-router-dom";
-import Particles from "../../components/Particles/MyParticles";
 import cv from "../../assets/documents/CV.pdf";
+import ukuleleBg from "../../assets/my_pictures/chosen/ukulele_3.webp";
 
 const Home = () => {
   return (
-    <div className="home-div">
-      <div className="canvas-absolute">
-        <Particles />
-      </div>
-      <div className="welcome-div">
-        <h4>
-          Hello there! <br />
-          Learn more about me by clicking
-          <b>
-            <NavLink to="/about"> here</NavLink>
-          </b>
-        </h4>
-        <a href={cv} target="_blank" rel="noreferrer">
-          <button className="cv-button">
-            Download my CV
-            <div className="download-icon">
-              <i className="fas fa-cloud-download-alt"></i>
-            </div>
-          </button>
-        </a>
+    <div className="home-bg" style={{ backgroundImage: `url(${ukuleleBg})` }}>
+      <div className="home-overlay">
+        <div className="home-content">
+          <h1>
+            Hi, I'm Ula <span className="wave">👋</span>
+          </h1>
+          <p>
+            Aspiring IT Project Manager ready to lead innovation in tech.
+          </p>
+          <div className="home-buttons">
+            <a href={cv} target="_blank" rel="noopener noreferrer">
+              <button className="cv-btn">
+                <i className="fas fa-file-download"></i> Download my CV
+              </button>
+            </a>
+            <a href="/about">
+              <button className="story-btn">Discover my story</button>
+            </a>
+          </div>
+          <ul className="home-key-points">
+            <li>3+ years in IT project management</li>
+            <li>Managed €950K+ budgets</li>
+            
+          </ul>
+        </div>
       </div>
     </div>
   );
